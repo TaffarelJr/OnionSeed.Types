7c9b8135456cdca2819d0adb508d46625b5097d8
RJ Hollberg <rj.hollberg@outlook.com>
2019-05-07 09:23:37 -0600

    Add Interval class and tests.

---
+++ src/OnionSeed.Types/Interval.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using OnionSeed.Helpers.Comparable;
+
+namespace OnionSeed.Types
+{
+	/// <summary>
+	/// Represents a mathematical interval. This can be useful for things like date ranges.
+	/// </summary>
+	/// <typeparam name="T">The type of values in the interval.</typeparam>
+	/// <remarks>Details on mathematical intervals can be found here: https://en.wikipedia.org/wiki/Interval_(mathematics) .</remarks>
+	public struct Interval<T> : IEquatable<Interval<T>>
+		where T : IComparable<T>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Interval{T}"/> struct.
+		/// </summary>
+		/// <param name="min">The minimum (left) endpoint in the interval.</param>
+		/// <param name="max">The maximum (right) endpoint in the interval.</param>
+		/// <remarks>By default, the interval will be closed (inclusive).</remarks>
+		public Interval(T min, T max)
+			: this(true, min, max, true)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Interval{T}"/> struct.
+		/// </summary>
+		/// <param name="minIsIncluded">A value indicating whether the <see cref="Min"/> endpoint is included in the interval.</param>
+		/// <param name="min">The minimum (left) endpoint in the interval.</param>
+		/// <param name="max">The maximum (right) endpoint in the interval.</param>
+		/// <param name="maxIsIncluded">A value indicating whether the <see cref="Max"/> endpoint is included in the interval.</param>
+		public Interval(bool minIsIncluded, T min, T max, bool maxIsIncluded)
+		{
+			if (max.IsLessThan(min))
+				throw new ArgumentOutOfRangeException(nameof(max), "The Max endpoint cannot be less than the Min endpoint.");
+
+			MinIsIncluded = minIsIncluded;
+			Min = min;
+			Max = max;
+			MaxIsIncluded = maxIsIncluded;
+		}
+
+		/// <summary>
+		/// Gets the minimum (left) endpoint in the interval.
+		/// </summary>
+		public T Min { get; }
+
+		/// <summary>
+		/// Gets the maximum (right) endpoint in the interval.
+		/// </summary>
+		public T Max { get; }
+
+		/// <summary>
+		/// Gets a value indicating whether the <see cref="Min"/> endpoint is included in the interval.
+		/// </summary>
+		public bool MinIsIncluded { get; }
+
+		/// <summary>
+		/// Gets a value indicating whether the <see cref="Max"/> endpoint is included in the interval.
+		/// </summary>
+		public bool MaxIsIncluded { get; }
+
+		/// <summary>
+		/// Returns <c>true</c> if the operands are equal, otherwise <c>false</c>.
+		/// </summary>
+		/// <param name="left">The first operand.</param>
+		/// <param name="right">The second operand.</param>
+		/// <returns><c>true</c> if the operands are equal, otherwise <c>false</c>.</returns>
+		public static bool operator ==(Interval<T> left, Interval<T> right)
+		{
+			return left.Equals(right);
+		}
+
+		/// <summary>
+		/// Returns <c>false</c> if the operands are equal, otherwise <c>true</c>.
+		/// </summary>
+		/// <param name="left">The first operand.</param>
+		/// <param name="right">The second operand.</param>
+		/// <returns><c>false</c> if the operands are equal, otherwise <c>true</c>.</returns>
+		public static bool operator !=(Interval<T> left, Interval<T> right)
+		{
+			return !left.Equals(right);
+		}
+
+		/// <inheritdoc />
+		public override bool Equals(object obj)
+		{
+			var other = obj as Interval<T>?;
+			return other.HasValue
+				? Equals(other.Value)
+				: false;
+		}
+
+		/// <inheritdoc />
+		public bool Equals(Interval<T> other)
+		{
+			return Min.IsEqualTo(other.Min)
+				&& Max.IsEqualTo(other.Max)
+				&& MinIsIncluded == other.MinIsIncluded
+				&& MaxIsIncluded == other.MaxIsIncluded;
+		}
+
+		/// <inheritdoc />
+		public override int GetHashCode()
+		{
+			return HashCode
+				.Of(MinIsIncluded)
+				.And(Min)
+				.And(Max)
+				.And(MaxIsIncluded);
+		}
+
+		/// <summary>
+		/// Returns the string representation of this instance.
+		/// </summary>
+		/// <returns>The string representation of this instance.</returns>
+		public override string ToString()
+		{
+			var start = MinIsIncluded ? '[' : '(';
+			var end = MaxIsIncluded ? ']' : ')';
+			return $"{start}{Min}, {Max}{end}";
+		}
+	}
+}
---
+++ test/OnionSeed.Types.Tests/IntervalTests.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace OnionSeed.Types
+{
+	public class IntervalTests
+	{
+		[Theory]
+		[InlineData(3, 3)]
+		[InlineData(3, 4)]
+		[InlineData(3, 7)]
+		public void Constructor_ShouldCreateInstance(int min, int max)
+		{
+			// Act
+			var result = new Interval<int>(min, max);
+
+			// Assert
+			result.MinIsIncluded.Should().BeTrue();
+			result.Min.Should().Be(min);
+			result.Max.Should().Be(max);
+			result.MaxIsIncluded.Should().BeTrue();
+		}
+
+		[Theory]
+		[InlineData(3, 2)]
+		public void Constructor_ShouldThrowException_WhenMaxIsLessThanMin(int min, int max)
+		{
+			// Act
+			Action action = () => new Interval<int>(min, max);
+
+			// Assert
+			action.Should().Throw<ArgumentOutOfRangeException>();
+		}
+
+		[Theory]
+		[InlineData(false, 3, 3, false)]
+		[InlineData(false, 3, 7, false)]
+		[InlineData(false, 3, 3, true)]
+		[InlineData(false, 3, 7, true)]
+		[InlineData(true, 3, 3, false)]
+		[InlineData(true, 3, 7, false)]
+		[InlineData(true, 3, 3, true)]
+		[InlineData(true, 3, 7, true)]
+		public void Constructor_ShouldCreateInstance_WhenInclusionIsSpecified(bool includeMin, int min, int max, bool includeMax)
+		{
+			// Act
+			var result = new Interval<int>(includeMin, min, max, includeMax);
+
+			// Assert
+			result.MinIsIncluded.Should().Be(includeMin);
+			result.Min.Should().Be(min);
+			result.Max.Should().Be(max);
+			result.MaxIsIncluded.Should().Be(includeMax);
+		}
+
+		[Theory]
+		[InlineData(false, 3, 2, false)]
+		[InlineData(false, 3, 2, true)]
+		[InlineData(true, 3, 2, false)]
+		[InlineData(true, 3, 2, true)]
+		public void Constructor_ShouldThrowException_WhenMaxIsLessThanMin_AndInclusionIsSpecified(bool includeMin, int min, int max, bool includeMax)
+		{
+			// Act
+			Action action = () => new Interval<int>(includeMin, min, max, includeMax);
+
+			// Assert
+			action.Should().Throw<ArgumentOutOfRangeException>();
+		}
+
+		[Theory]
+		[InlineData(false, 3, 7, false, true)]
+		[InlineData(false, 3, 7, true, false)]
+		[InlineData(false, 3, 6, false, false)]
+		[InlineData(false, 4, 7, false, false)]
+		[InlineData(true, 3, 7, false, false)]
+		public void EqualityOperator_ShouldReturnCorrectResult(bool includeMin, int min, int max, bool includeMax, bool expected)
+		{
+			// Arrange
+			var left = new Interval<int>(false, 3, 7, false);
+			var right = new Interval<int>(includeMin, min, max, includeMax);
+
+			// Act
+			var result = left == right;
+
+			// Assert
+			result.Should().Be(expected);
+		}
+
+		[Theory]
+		[InlineData(false, 3, 7, false, false)]
+		[InlineData(false, 3, 7, true, true)]
+		[InlineData(false, 3, 6, false, true)]
+		[InlineData(false, 4, 7, false, true)]
+		[InlineData(true, 3, 7, false, true)]
+		public void InequalityOperator_ShouldReturnCorrectResult(bool includeMin, int min, int max, bool includeMax, bool expected)
+		{
+			// Arrange
+			var left = new Interval<int>(false, 3, 7, false);
+			var right = new Interval<int>(includeMin, min, max, includeMax);
+
+			// Act
+			var result = left != right;
+
+			// Assert
+			result.Should().Be(expected);
+		}
+
+		[Theory]
+		[InlineData(null)]
+		[InlineData("hello")]
+		public void Equals_ShouldReturnFalse_WhenTypeIsWrong(string other)
+		{
+			// Arrange
+			var interval = new Interval<int>(3, 7);
+
+			// Act
+			var result = interval.Equals(other);
+
+			// Assert
+			result.Should().BeFalse();
+		}
+
+		[Theory]
+		[InlineData(false, 3, 7, false, true)]
+		[InlineData(false, 3, 7, true, false)]
+		[InlineData(false, 3, 6, false, false)]
+		[InlineData(false, 4, 7, false, false)]
+		[InlineData(true, 3, 7, false, false)]
+		public void Equals_ShouldReturnCorrectResult_WhenTypeIsCorrect(bool includeMin, int min, int max, bool includeMax, bool expected)
+		{
+			// Arrange
+			var interval = new Interval<int>(false, 3, 7, false);
+			var other = new Interval<int>(includeMin, min, max, includeMax);
+
+			// Act
+			var result = interval.Equals(other);
+
+			// Assert
+			result.Should().Be(expected);
+		}
+
+		[Fact]
+		public void GetHashCode_ShouldReturnUniqueValues()
+		{
+			// Arrange
+			var subject1 = new Interval<int>(false, 3, 7, false);
+			var subject2 = new Interval<int>(false, 3, 7, true);
+			var subject3 = new Interval<int>(false, 3, 6, false);
+			var subject4 = new Interval<int>(false, 4, 7, false);
+			var subject5 = new Interval<int>(true, 3, 7, false);
+
+			// Act
+			var result1 = subject1.GetHashCode();
+			var result2 = subject2.GetHashCode();
+			var result3 = subject3.GetHashCode();
+			var result4 = subject4.GetHashCode();
+			var result5 = subject5.GetHashCode();
+
+			// Assert
+			result1.Should().NotBe(0);
+			result2.Should().NotBe(0);
+			result3.Should().NotBe(0);
+			result4.Should().NotBe(0);
+			result5.Should().NotBe(0);
+
+			result1.Should().NotBe(result2);
+			result1.Should().NotBe(result3);
+			result1.Should().NotBe(result4);
+			result1.Should().NotBe(result5);
+
+			result2.Should().NotBe(result3);
+			result2.Should().NotBe(result4);
+			result2.Should().NotBe(result5);
+
+			result3.Should().NotBe(result4);
+			result3.Should().NotBe(result5);
+
+			result4.Should().NotBe(result5);
+		}
+
+		[Theory]
+		[InlineData(false, 3, 7, false, "(3, 7)")]
+		[InlineData(false, 3, 7, true, "(3, 7]")]
+		[InlineData(true, 3, 7, false, "[3, 7)")]
+		[InlineData(true, 3, 7, true, "[3, 7]")]
+		public void ToString_ShouldReturnCorrectRepresentation(bool includeMin, int min, int max, bool includeMax, string expected)
+		{
+			// Arrange
+			var subject = new Interval<int>(includeMin, min, max, includeMax);
+
+			// Act
+			var result = subject.ToString();
+
+			// Assert
+			result.Should().Be(expected);
+		}
+	}
+}
